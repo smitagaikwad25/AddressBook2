58340d59baff102745fb5aef5f5b3e4ce18b0ec0
smitagaikawad25 <gaikawadsmita021@gmail.com>
2020-05-27 19:43:25 +0530

    refactored code remove code  which was not requied

--- main/service/addressbook.service.js
+++ main/service/addressbook.service.js
@@ -5,10 +5,7 @@ module.exports = {
     createAddressBookRecord(userDetail, callback) {
         var data = fs.readFileSync('main/uitility/addressbook.json')
         var json = JSON.parse(data);
-        console.log("data before push---->", json);
-
         json.push(userDetail)
-        console.log("data after push---->", json);
 
         fs.writeFile('main/uitility/addressbook.json', JSON.stringify(json, null, 4), ((err, data) => {
             if (err) {
--- test/addressbooktest.js
+++ test/addressbooktest.js
@@ -9,16 +9,17 @@ var addressbookData = fs.readFileSync('main/uitility/addressbook.js')
 
 describe('Test Cases for first api type', () => {
 
-    it('given details should return status code 200', (done) => {
+    it('given details is in proper form should return status code 200', (done) => {
         chai.request(server)
             .post('/createrecord')
-            .send({firstName:"smita",
-                lastName:"shinde",
-                address:"thane",
-                city:"thane",
-                state:"maharastra",
-                zip:606060,
-                phoneNumber:0987654321
+            .send({
+                firstName: "smita",
+                lastName: "shinde",
+                address: "thane",
+                city: "thane",
+                state: "maharastra",
+                zip: 606060,
+                phoneNumber: 0987654321
             })
             .end((err, res) => {
                 res.should.have.status(200);
@@ -26,5 +27,75 @@ describe('Test Cases for first api type', () => {
             });
     });
 
+    it('given http method when wrong return status code 404', (done) => {
+        chai.request(server)
+            .put('/createrecord')
+            .send({
+                firstName: "smita",
+                lastName: "shinde",
+                address: "thane",
+                city: "thane",
+                state: "maharastra",
+                zip: 606060,
+                phoneNumber: 0987654321
+            })
+            .end((err, res) => {
+                res.should.have.status(404);
+                done();
+            });
+    });
+
+    it('given http path when wrong return status code 404', (done) => {
+        chai.request(server)
+            .post('/creadte')
+            .send({
+                firstName: "smita",
+                lastName: "shinde",
+                address: "thane",
+                city: "thane",
+                state: "maharastra",
+                zip: 606060,
+                phoneNumber: 0987654321
+            })
+            .end((err, res) => {
+                res.should.have.status(404);
+                done();
+            });
+    });
+
+    it('given pran when wrong return status code 500', (done) => {
+        chai.request(server)
+            .post('/createrecord')
+            .send({
+                firstname: "smita",
+                lastName: "shinde",
+                address: "thane",
+                city: "thane",
+                state: "maharastra",
+                zip: 606060,
+                phoneNumber: 0987654321
+            })
+            .end((err, res) => {
+                res.should.have.status(500);
+                done();
+            });
+    });
+
+    it('given user detail when with missing pram return status code 500', (done) => {
+        chai.request(server)
+            .post('/createrecord')
+            .send({
+                lastName: "shinde",
+                address: "thane",
+                city: "thane",
+                state: "maharastra",
+                zip: 606060,
+                phoneNumber: 0987654321
+            })
+            .end((err, res) => {
+                res.should.have.status(500);
+                done();
+            });
+    });
 
 })
