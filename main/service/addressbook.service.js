42cfe7bf416132054016ee662f581eee7a27a8ad
smitagaikawad25 <gaikawadsmita021@gmail.com>
2020-05-27 15:28:49 +0530

    Refactored removed commented code from service

--- main/service/addressbook.service.js
+++ main/service/addressbook.service.js
@@ -5,29 +5,17 @@ module.exports = {
     createAddressBookRecord(userDetail, callback) {
 
         console.log("userDetail at service ---->", typeof userDetail);
-        // addressBook.push(userDetail)
 
-        // console.log("while pushing data into file--->", addressBook);
+        const finalData = JSON.stringify(userDetail)
 
-        // const finalData = JSON.stringify(addressBook)
-
-        // console.log("data after push--->", finalData);
-        // console.log("singifdata--->",JSON.stringify(userDetail));
-        //  const finalData = JSON.stringify(userDetail)
-        //  console.log("singifdata--->",JSON.parse(finalData));
-
-        fs.writeFileSync('main/uitility/addressbook.json', userDetail, ((err, data) => {
+        fs.writeFileSync('main/uitility/addressbook.json', finalData, ((err, data) => {
             if (err) {
-                console.log("erro---->", err);
-
+                console.log("erro---->", err)
                 return callback(err, null)
             }
-            console.log("data--->", data);
 
-
-            return callback(null, data)
-
+            return callback({ message: "successfully done" })
         }))
-    }
+     }
 }
 
