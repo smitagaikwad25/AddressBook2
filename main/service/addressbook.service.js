285f9c345d8d3fce2b8d0f43f3a9ba2de819bd35
smitagaikawad25 <gaikawadsmita021@gmail.com>
2020-06-05 19:55:17 +0530

    Refactored code to remove console log from service

--- main/service/addressbook.service.js
+++ main/service/addressbook.service.js
@@ -5,12 +5,9 @@ module.exports = {
     createAddressBookRecord(userDetail, callback) {
         var data = fs.readFileSync('main/uitility/addressbook.json')
         var json = JSON.parse(data);
-        console.log("data befor push--->", json);
         json.push(userDetail)
-        console.log("data after push--->", json);
         fs.writeFile('main/uitility/addressbook.json', JSON.stringify(json, null, 4), ((err, data) => {
             if (err) {
-                console.log("erro---->", err)
                 return callback(err, null)
             }
             return callback(null, { message: "successfully done" })
